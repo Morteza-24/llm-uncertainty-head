724deac6371a8eb4e15f47b37c11d42205b2c701
morteza24 <mortezia4444.mz@gmail.com>
2026-08-19 11:10:41 +0330

    deleted unused files

--- luh/builder_CalculatorCachedPredictions.py
+++
@@ -1,7 +0,0 @@
-from .calculator_cached_predictions import CalculatorCachedPredictions
-
-
-def load_stat_calculator(config, builder):
-    calc = CalculatorCachedPredictions(config.cached_predictions_path, config.dataset_path)
-    return calc
-
